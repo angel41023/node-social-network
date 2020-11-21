67453204a804d7a448824ea4df72f023d4baba58
Angel Rodriguez <angel41023@hotmail.com>
2020-11-20 20:12:54 -0600

    Refactor user errors

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -6,54 +6,44 @@ const Controller = require('./index')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   Controller.list()
     .then((data) => {
       response.success(req, res, data, 200)
     })
-    .catch(err => {
-      response.error(req, res, err.message, 500)
-    })
+    .catch(next)
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   Controller.get(req.params.id)
     .then((user) => {
       response.success(req, res, user, 200)
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500)
-    })
+    .catch(next)
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   Controller.create(req.body.user)
     .then((user) => {
       response.success(req, res, user, 200)
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500)
-    })
+    .catch(next)
 })
 
-router.patch('/:id', secure('update'), (req, res) => {
+router.patch('/:id', secure('update'), (req, res, next) => {
   Controller.update(req.params.id, req.body.name)
     .then((user) => {
       response.success(req, res, user, 201)
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500)
-    })
+    .catch(next)
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   Controller.remove(req.params.id)
     .then((user) => {
       response.success(req, res, user, 201)
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500)
-    })
+    .catch(next)
 })
 
 module.exports = router

490c5c5c199560fd23b1419ef44e5d0a6750b711
Angel Rodriguez <angel41023@hotmail.com>
2020-11-28 12:33:46 -0600

    List posts created

---
+++ api/components/post/network.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const response = require('../../../network/response')
+const Controller = require('./index')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  Controller.list()
+    .then(data => {
+      response.success(req, res, data, 200)
+    })
+    .catch(next)
+})
+
+module.exports = router

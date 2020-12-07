413333bbbecf000eb66c44be790b1cfe0de95d31
Angel Rodriguez <angel41023@hotmail.com>
2020-12-07 13:45:37 -0600

    list users connected with redis cache

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,6 @@
 // const store = require('../../../store/mysql')
 const store = require('../../../store/remote-mysql')
+const cache = require('../../../store/remote-cache')
 const ctrl = require('./controller')
 
-module.exports = ctrl(store)
+module.exports = ctrl(store, cache)
